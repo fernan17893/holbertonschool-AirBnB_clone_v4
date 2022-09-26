290c17c9b69823574c86d52da56c464317d3c618
Xavier <xavierperez1599@gmail.com>
2022-09-25 21:55:04 -0400

    adding changes to task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-import uuid
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,8 +7,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-
-app = Flask(__name__)
+import uuid
+app = Flask(__name__, template_folder='template')
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -20,25 +19,29 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/')
-def hbnb_filters(the_id=None):
-    """
-    handles request to custom templates
-    """
-    state_objs = storage.all('State').values()
-    states = dict([state.name, state] for state in state_objs)
-    amens = storage.all('Amenity').values()
-    places = storage.all('Place').values()
-    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-            for user in storage.all('User').values())
+@app.route('/0-hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
     return render_template('0-hbnb.html',
-            states=states,
-            amens=amens,
-            places=places,
-            users=users,
-            cache_id=uuid.uuid4())
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 
-    if __name__ == "__main__":
-        """ Main Function """
-    app.run(host=host, port=port, debug=true)
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)

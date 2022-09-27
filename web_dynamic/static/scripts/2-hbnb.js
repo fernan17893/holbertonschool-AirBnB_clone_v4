7df2b06686dc729bccdc4188f1044b150b2ef3bb
Xavier <xavierperez1599@gmail.com>
2022-09-27 12:07:00 -0400

    pushed new files and changes

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,12 @@
+$('document').ready(function () {
+    let amenities = {};
+    $('.amenities :checkbox').change(function () {
+        if ($(this).prop("checked")) {
+            amenities[$(this).data().id] = $(this).data().name;
+        } else {
+            delete amenities[$(this).attr('data-id')];
+        }
+        console.log($(this).data());
+        $('.amenities h4').text(Object.values(amenities).join(', '));
+    });
+});
